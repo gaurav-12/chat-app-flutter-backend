a16f7cda859e5e7ece38552db4f92f5b3d0bdec7
Gaurav <gaurav.12apr@gmail.com>
2020-04-30 03:20:42 +0530

    Added more log messages

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const http = require('http').createServer(app);
 
 app.get('/', (req, res) => {
     res.send("Node server is running...");
-    console.log("Request received");
+    console.log("Node server started...");
 })
 
 const socket = require('socket.io')(http);
 
 socket.on('connection', (userSocket) => {
+    console.log('Connected now!');
+
     userSocket.on('send_message', (data) => {
+        console.log("Received message...now broadcasting...");
         userSocket.broadcast.emit('receive_message', data);
     })
 })
